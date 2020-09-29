6bb7e084d86e31d06ee972b1edcdddf038010476
Hank DeDona <icon5585@gmail.com>
2020-09-29 10:36:49 -0500

    Refactoring

---
+++ CSharp/LC101-Unit2/Class-2.1/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Class_2._1
+{
+    public class Cat
+    {
+        String name;
+
+
+        public Cat()
+        {
+            name = "cat";
+        }
+
+        public Cat(String name)
+        {
+            this.name = name;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        // 2.4.2 Static Methods
+        public static void makeNoise(String noise)
+        {
+            if(noise == null) {
+                Console.WriteLine("Roar!");
+            }
+            else
+            {
+                Console.WriteLine(noise);
+            }
+        }
+                
+    }
+}
---
+++ CSharp/LC101-Unit2/Class-2.2/Lecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/**
+ * LC 101 - Unit 2 
+ * Class 2.2
+ * Chapter 3 - Control Flow & Collections
+ * Hank DeDona
+ * 10/01/20
+ */
+namespace Class_2._2
+{
+    class Lecture
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
