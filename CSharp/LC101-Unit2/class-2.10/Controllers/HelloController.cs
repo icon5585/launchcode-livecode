ab5cbebbe8a5f9aa9bfb193bdf751cb7299c03a0
Hank DeDona <icon5585@gmail.com>
2020-10-27 19:08:30 -0500

    Added class-2.10

---
+++ CSharp/LC101-Unit2/class-2.10/Controllers/HelloController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace class_2._10.Controllers
+{
+    // 10.1.2.1 In order to designate this class as a controller, it must extend
+    // the abstract class "Controller"
+    // 10.4.2.1 Class-Level Attributes
+    // You can specify a "base route" for the entire class, all methods will now be at /helloworld/...
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+
+        // 10.1.2.1/2 Controllers/mappings
+        // GET: /<controller>/helloworld
+        // We use attributes to define what this method in this controller does
+        // GET (can also do POST, PUT or DELETE)
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            // This allows us to return static text/html directly in the controller using the Content object
+            // and will return "Hello World!" in h1 (header 1)
+            return Content("<h1>" + "Hello World!" + "<h1>", "text/html");
+        }
+
+        // 10.2.3. Controllers and Parameters
+        // GET: /<controller>/welcome
+        [HttpGet("welcome/{name?}")]
+        // POST: /<controller>/welcome
+        [HttpPost("welcome")]
+        // Adding a string parameter to the welcome method specifies a query param, ironically named name :P
+        // [Route("/welcome/{name?}")]
+        public IActionResult Welcome(string name = "LaunchCode" /* this specifies a default value, making name optional */)
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+
+        // 10.3.2 Sending form data
+        [HttpGet]
+        [Route("/helloworldform")]
+        public IActionResult GetHelloWorldForm()
+        {
+            // This will return a simple html form
+            // When submitted it will do a POST to the /helloworld/welcome method
+            string html = "<form method='post' action='/helloworld/welcome'>" +
+               "<input type='text' name='name' />" +
+               "<input type='submit' value='Greet Me!' />" +
+               "</form>";
+
+            return Content(html, "text/html");
+        }
+
+    }
+}
--- CSharp/LC101-Unit2/class-2.10/Program.cs
+++ CSharp/LC101-Unit2/class-2.10/Program.cs
@@ -33,7 +33,60 @@ namespace class_2._10
 
         private static void Lecture()
         {
+            // 10.1 Simple Controllers
+            // Controllers, the traffic cops of web applications/services
 
+            // 10.1.1 Controllers and static responses
+            // Controllers are found in the "Controllers" directory of your ASP.net core project
+            // Note: Surprise, views are in the "Views" directory and models in the "Models" directory
+
+            // Let's create our first controller, refer to HelloController.cs in this project
+            // The routing pattern of controllers is controller/action, e.g. Home/Index (this is in the HomeController.cs
+
+            // In order to specify the HTTP method supported by your controller method, we use attributes
+            // GET [HttpGet]
+            // POST [HttpPost]
+            // PUT [HttpPut]
+            // DELETE [HttpDelete]
+
+            // In order to specify the route, we need another attribute
+            // [Route("/helloworld")]
+
+            // For our home controller we will specify the HTTP method (in this case GET) as well as the
+            // path (in this case HelloWorld)
+            // So the full URL would be https://localhost:{port}/helloworld
+
+            // 10.2 Controllers with parameters
+            // 10.2.1 Query strings - These are additional request parameters that are appended to your request URL
+            // separated by the question mark "?" and delimited by the ampersand "&"
+
+            // In order to add a query param to your controller method, you simply add a string variable to the parameters
+            // The new URL for the welcome (passing in name) would be https://localhost:{port}/helloworld/welcome?name=milo
+            // See Welcome() method in the HelloController.cs
+
+            // 10.2.3.2 Path variables
+            // We can use the [Route] attribute to specify path variables (parameters)
+            // By putting the string "name" in curly brackets, it tells us that it's a variable, the "?" specifies that it's optional
+            // e.g. [Route("/helloworld/welcome/{name?}")]
+
+            // The new URL for the path-based welcome URL would be https://localhost:{port}/helloworld/welcome/milo
+
+            // 10.3 Controllers with Forms
+            // Refer to GetHelloWorldForm() in HelloController.cs
+
+            // 10.4.2 Cleaning up the controller
+            // You can specify routes at the class-level
+            // e.g. [Route("/helloworld")]
+            // This will say that ALL methods in this class will be at /helloworld/...
+
+            // 10.4.2.2. One Method, Two Request Types
+            // You can specify one method to handle multiple HTTP methods (e.g. GET and POST)
+            // The HttpGet and other method attributes can also take in the path
+            // For example: 
+            // [HttpGet("welcome/{name?}")]
+            // [HttpPost("welcome")]
+
+            // This removes the need for the [Route] attribute
         }
     }
 }
